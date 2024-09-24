f88fd46d347df1eb7f66fd90d1db1c65cc69d05d
pradeeppandey99 <37412150+pradeeppandey99@users.noreply.github.com>
2024-09-24 16:17:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -121,16 +121,16 @@ function calculateManpower() {
         let additionalDoctors = 0;
         if (hireOption.value === 'registrars') {
             additionalRegistrars = Math.abs(totalDeficitSurplusManhours) / (assumptions.registrarTime * assumptions.manhours);
-            resultDiv.innerHTML += `<p>Additional registrars required: ${additionalRegistrars.toFixed(2)}</p>`;
+            resultDiv.innerHTML += `<p class="additional-result">Additional registrars required: ${additionalRegistrars.toFixed(0)}</p>`;
         } else if (hireOption.value === 'doctors') {
             additionalDoctors = Math.abs(totalDeficitSurplusManhours) / (assumptions.consultantTime * assumptions.manhours);
-            resultDiv.innerHTML += `<p>Additional doctors required: ${additionalDoctors.toFixed(2)}</p>`;
+            resultDiv.innerHTML += `<p class="additional-result">Additional doctors required: ${additionalDoctors.toFixed(0)}</p>`;
         } else if (hireOption.value === 'mix') {
             const additionalRegistrarsInput = parseFloat(document.getElementById('additionalRegistrars').value) || 0;
             const remainingManhours = Math.abs(totalDeficitSurplusManhours) - (additionalRegistrarsInput * assumptions.registrarTime * assumptions.manhours);
             additionalDoctors = remainingManhours / (assumptions.consultantTime * assumptions.manhours);
-            resultDiv.innerHTML += `<p>Additional registrars required: ${additionalRegistrarsInput.toFixed(2)}</p>`;
-            resultDiv.innerHTML += `<p>Additional doctors required: ${additionalDoctors.toFixed(2)}</p>`;
+            resultDiv.innerHTML += `<p class="additional-result">Additional registrars required: ${additionalRegistrarsInput.toFixed(0)}</p>`;
+            resultDiv.innerHTML += `<p class="additional-result">Additional doctors required: ${additionalDoctors.toFixed(0)}</p>`;
         }
     }
 }
